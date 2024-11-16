bb262ce5dc770776b7f0e3488859cb96f138e9cd
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-11-16 14:44:18 +0530

    loader added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import TodoForm from "../components/TodoForm/TodoForm";
 import TodoList from "../components/TodoList/TodoList";
 import { useTodosContext } from "../hooks/useTodosContext";
 import relaxIcon from "../assets/relax-icon.svg";
 import { useAuthContext } from "../hooks/useAuthContext";
+import loaderPlant from "../assets/loader-plant.gif";
 import "./home.css";
 
 const Home = () => {
+  const [areTodosLoading, setAreTodosLoading] = useState(false);
+
   const { todos, dispatch } = useTodosContext();
   const { user } = useAuthContext();
   useEffect(() => {
     const fetchTodos = async () => {
+      setAreTodosLoading(true);
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/api/v1/todos`,
         {
@@ -22,6 +26,7 @@ const Home = () => {
       const json = await response.json();
 
       if (response.ok) {
+        setAreTodosLoading(false);
         dispatch({ type: "SET_TODOS", payload: json.data.todos });
       }
     };
@@ -34,12 +39,12 @@ const Home = () => {
   return (
     <main className={todos?.length > 0 ? "" : "main-empty"}>
       <TodoForm />
-      {todos?.length > 0 ? (
+      {todos?.length > 0 && !areTodosLoading ? (
         <div>
           <h2 className="title">Your Todos</h2>
           <TodoList todos={todos} />
         </div>
-      ) : (
+      ) : !todos?.length > 0 && !areTodosLoading ? (
         <div className="relax-container">
           <img className="relax-icon" src={relaxIcon} alt="" />
           <p className="relax-text">
@@ -47,7 +52,12 @@ const Home = () => {
             Enjoy your coffee time!
           </p>
         </div>
+      ) : (
+        <div className="relax-container">
+          <img src={loaderPlant} className="loader" alt="" />
+        </div>
       )}
+
       {/* {console.log(todos)} */}
     </main>
   );
