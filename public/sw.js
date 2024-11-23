21b7a709b99c93ba6e780f64668b191747ca61e1
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-11-23 20:16:24 +0530

    added notification icon

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,29 @@ self.addEventListener("push", (event) => {
   const data = event.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
-    // icon: "/icon.png", // Add an appropriate icon
+    icon: data.icon, // Add an appropriate icon
   });
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close(); // Close the notification
+
+  // Define the URL to redirect the user to
+  const url = "https://tickit-todo.netlify.app/";
+
+  // Focus on an existing tab with the web app or open a new one
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+      })
+  );
 });
