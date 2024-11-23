2dbfc538dea09f54070d7608ce3af649c5c480bc
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-11-23 21:56:42 +0530

    notification icon added to client

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@ self.addEventListener("push", (event) => {
   const data = event.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: data.icon, // Add an appropriate icon
+    icon: "https://tickit-todo.netlify.app/notification-icon.png",
   });
 });
 
