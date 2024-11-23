a8645050b83d40f9215f34a8daa9bd7268b9e5b2
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-11-23 20:35:10 +0530

    added redirection from notification

--- public/sw.js
+++ public/sw.js
@@ -7,24 +7,9 @@ self.addEventListener("push", (event) => {
 });
 
 self.addEventListener("notificationclick", (event) => {
-  event.notification.close(); // Close the notification
+  // Close the notification when clicked
+  event.notification.close();
 
-  // Define the URL to redirect the user to
-  const url = "https://tickit-todo.netlify.app/";
-
-  // Focus on an existing tab with the web app or open a new one
-  event.waitUntil(
-    clients
-      .matchAll({ type: "window", includeUncontrolled: true })
-      .then((clientList) => {
-        for (const client of clientList) {
-          if (client.url === url && "focus" in client) {
-            return client.focus();
-          }
-        }
-        if (clients.openWindow) {
-          return clients.openWindow(url);
-        }
-      })
-  );
+  // Handle notification click (e.g., open a specific page)
+  event.waitUntil(clients.openWindow("/"));
 });
